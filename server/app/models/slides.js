c842869b2f5f4229541cc5aed3abbda59c1d62bf
alex-bu-89 <alex@mycs.com>
2016-10-08 14:44:07 +0200

    clear files

--- server/app/models/slides.js
+++ server/app/models/slides.js
@@ -5,30 +5,6 @@ const shortid = require('shortid');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-/*var AllAnswersSchema = new Schema({
- answer: { type: String, default: '' },
- });
-
- var TasksSchema = new Schema({
- type: { type: String, default: '' },
- code: { type: String, default: '' },
- answer: { type: String, default: '' },
- allAnswers: { type: Schema.Types.ObjectId, ref: 'AllAnswers' },
- });
-
- var SlidesSchema = new Schema({
- content: { type: String, default: '' },
- task: { type: Schema.Types.ObjectId, ref: 'Tasks' },
- assets: { type: String, default: '' },
- });
-
- var ThemeSchema = new Schema({
- _id: { type: String, default: shortid.generate },
- title: { type: String, default: '' },
- subject: { type: String, default: '' },
- slides: { type: Schema.Types.ObjectId, ref: 'Slide' },
- });*/
-
 var SlidesSchema = new Schema({
   _id: {type: String, default: shortid.generate},
   title: {type: String, default: ''},
@@ -40,7 +16,7 @@ var SlidesSchema = new Schema({
       questions: {type: String},
       code: {type: String},
       poll: {
-        possibleAnswers:[{
+        possibleAnswers: [{
           title: {type: String},
           votes: {type: Number}
         }]
@@ -49,21 +25,10 @@ var SlidesSchema = new Schema({
         answer: {type: Schema.Types.Mixed}
       }]
     },
-    assets: {type: String, default: ''},
-  }],
+    assets: {type: String, default: ''}
+  }]
 });
-/*
-poll: {
-  id: '3fd9f',
-    
-}*/
 
-/**
- * Register modul
- */
-// mongoose.model('Tasks', TasksSchema);
-// mongoose.model('AllAnswers', AllAnswersSchema);
 mongoose.model('Slides', SlidesSchema);
-// mongoose.model('Theme', ThemeSchema);
 
 
--- server/app/routes/slides.js
+++ server/app/routes/slides.js
@@ -1,114 +1,94 @@
 const mongoose = require('mongoose');
-/*
- const Tasks = mongoose.model('Tasks');
- const AllAnswers = mongoose.model('AllAnswers');
- const Theme = mongoose.model('Theme');
- */
-
 const Slides = mongoose.model('Slides');
 
-module.exports = function (app) {
+  module.exports = function (app) {
 
-  app.get('/slides', function (req, res) {
-    Slides.find({}, function (err, slides) {
-      res.json(slides);
-    });
-  });
-
-  app.get('/slides/:id', function (req, res) {
-    Slides.findOne({_id: req.params.id}, function (err, slides) {
-      res.json(slides);
-    });
-  });
-
-  app.post('/slides', function (req, res) {
-
-    // TODO remove
-    var slides = new Slides({
-      title: 'test',
-      subject: 'subject'
-    });
-    slides.slides = [
-      {
-        content: '<h1>Test structure from db</h1>',
-        task: {
-          type: 'code',
-          questions: 'What is',
-          code: "console.log('hello world')",
-          answer: ''
-        }
-      },
-      {
-        content: '<h1>Test structure from db</h1>',
-        task: {
-          type: 'poll',
-          questions: 'What\'s Your Favorite Programming Language?',
-          poll: {
-            possibleAnswers: [
-              {
-                title: 'Java',
-                votes: 0
-              },
-              {
-                title: 'C++',
-                votes: 0
-              },
-              {
-                title: 'JavaScript',
-                votes: 0
-              },
-              {
-                title: 'Rails',
-                votes: 0
-              }
-            ]
-          },
-          answer: ''
-        }
-      },
-      {
-        content: '<h1>Test structure</h1><p>Hello world</p>'
-      }
-    ];
-
-    slides.save(function (err) {
-      if (err) {
-        return res.json({success: false, err: err});
-      }
-      Slides.findOne({_id: slides._id}, function (err, slides) {
+    app.get('/slides', function (req, res) {
+      Slides.find({}, function (err, slides) {
         res.json(slides);
       });
     });
 
-  });
-
-  app.put('/slides/:id', function (req, res) {
-    console.log(req.params.id);
-    Slides.findOneAndUpdate({_id: req.params.id}, {$set: req.update}, {new: true}, function (err, doc) {
-      if (err) {
-        res.json({msg: "Something wrong when updating data!", e: err});
-      }
-      res.json(doc);
+    app.get('/slides/:id', function (req, res) {
+      Slides.findOne({_id: req.params.id}, function (err, slides) {
+        res.json(slides);
+      });
     });
-  });
 
-  app.delete('/slides/:id', function (req, res) {
-    console.log(req.params.id);
-    Slides.remove({_id: req.params.id}, function (err, removed) {
-      if (err) {
-        res.json({msg: "Something wrong when updating data!", e: err});
-      }
-      res.json({msg: "Successful", removed: removed});
+    app.post('/slides', function (req, res) {
+      var slides = new Slides({
+        title: 'test',
+        subject: 'subject'
+      });
+      slides.slides = [
+        {
+          content: '<h1>Test structure from db</h1>',
+          task: {
+            type: 'code',
+            questions: 'What is',
+            code: "console.log('hello world')",
+            answer: ''
+          }
+        },
+        {
+          content: '<h1>Test structure from db</h1>',
+          task: {
+            type: 'poll',
+            questions: 'What\'s Your Favorite Programming Language?',
+            poll: {
+              possibleAnswers: [
+                {
+                  title: 'Java',
+                  votes: 0
+                },
+                {
+                  title: 'C++',
+                  votes: 0
+                },
+                {
+                  title: 'JavaScript',
+                  votes: 0
+                },
+                {
+                  title: 'Rails',
+                  votes: 0
+                }
+              ]
+            },
+            answer: ''
+          }
+        },
+        {
+          content: '<h1>Test structure</h1><p>Hello world</p>'
+        }
+      ];
+      slides.save(function (err) {
+        if (err) {
+          return res.json({success: false, err: err});
+        }
+        Slides.findOne({_id: slides._id}, function (err, slides) {
+          res.json(slides);
+        });
+      });
     });
-  });
 
-  app.delete('/slides', function (req, res) {
-    /*Slides.remove({}, function (err, removed) {
-      if (err) {
-        res.json({msg: "Something wrong when updating data!", e: err});
-      }
-      res.json({msg: "Successful", removed: removed});
-    });*/
-  });
+    app.put('/slides/:id', function (req, res) {
+      Slides.findOneAndUpdate({_id: req.params.id}, {$set: req.update}, {new: true}, function (err, doc) {
+        if (err) {
+          res.json({msg: "Something wrong when updating data!", e: err});
+        }
+        res.json(doc);
+      });
+    });
 
-};
+    app.delete('/slides/:id', function (req, res) {
+      Slides.remove({_id: req.params.id}, function (err, removed) {
+        if (err) {
+          res.json({msg: "Something wrong when updating data!", e: err});
+        }
+        res.json({msg: "Successful", removed: removed});
+      });
+    });
+
+  };
+
